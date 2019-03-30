faa80c00586790745bde5d5ea7586e8d0cadec0d
Ross carmack <rosscarmack@Rosss-MBP.fios-router.home>
2019-03-29 20:47:15 -0500

    added check in page, login auth, navbars on all pages, appointment list/add form, and waiting room list along with all routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,24 +1,63 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("checkin", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+    db.Account.findAll({}).then(function(results) {
+      res.render("login", results);
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
+  app.get("/home", function(req, res) {
+    db.Appointment.findAll({}).then(function(results) {
+      if (req.session.loggedin) {
+        res.render("home", results);
+      } else {
+        res.render("login");
+      }
+    });
+  });
+
+  //patientDetails Page
+  app.get("/patients", function(req, res) {
+    db.Patient.findAll({}).then(function(results) {
+      if (req.session.loggedin) {
+        res.render("patientDetails", results);
+      } else {
+        res.render("login");
+      }
+    });
+  });
+
+  //appointments Page
+  app.get("/appointments", function(req, res) {
+    db.Appointment.findAll({}).then(function(results) {
+      if (req.session.loggedin) {
+        res.render("appointments", results);
+      } else {
+        res.render("login");
+      }
+    });
+  });
+
+  //checkin Page
+  app.get("/checkin", function(req, res) {
+    db.Appointment.findAll({}).then(function(results) {
+      res.render("checkin", results);
+    });
+  });
+
+  //waitingRoom Page
+  app.get("/waitingroom", function(req, res) {
+    db.Appointment.findAll({
+      where: {
+        isPresent: true
+      }
+    }).then(function(results) {
+      if (req.session.loggedin) {
+        res.render("waitingRoom", results);
+      } else {
+        res.render("login");
+      }
     });
   });
 
