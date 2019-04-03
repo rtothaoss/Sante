37ee97164a295567256fa1a7956a9565c716190f
Ross carmack <rosscarmack@Rosss-MacBook-Pro.local>
2019-04-02 19:28:45 -0500

    some styling changes

--- public/js/waitingRoom.js
+++ public/js/waitingRoom.js
@@ -4,6 +4,7 @@ $(function() {
   $.get("api/appointments", function(data) {
     for (var i = 0; i < data.length; i++) {
       if (data[i].isPresent === true) {
+        console.log(data);
         // var wellSection = $("<div>");
 
         // wellSection.append("<h2>" + "Patient Name: " + data[i].name + "</h2>");
@@ -18,6 +19,8 @@ $(function() {
 
         createNewRow.append("<td>" + data[i].name + "</td>");
         createNewRow.append("<td>" + data[i].reasonForVisit + "</td>");
+        // createNewRow.append("<button>" + "remove" + "</button>");
+        // createNewRow.attr("id", data[i].id);
 
         // deleteButton.attr('id', 'delete-button')
 
@@ -25,4 +28,24 @@ $(function() {
       }
     }
   });
+
+  // $.get("/api/appointments", function(data) {
+  //   console.log(data);
+  //   var rowsToAdd = [];
+
+  //   for (var i = 0; i < data.length; i++) {
+  //     if (data[i].isPresent === true) {
+  //       rowsToAdd.push(waitingRoomList(data[i]));
+  //     }
+  //   }
+  //   // console.log(rowsToAdd);
+  //   $("#waitingRoom").append(rowsToAdd);
+
+  //   function waitingRoomList(patient) {
+  //     var listOption = $("<option>");
+  //     listOption.attr("value", patient.id);
+  //     listOption.text(patient.name);
+  //     return listOption;
+  //   }
+  // });
 });
